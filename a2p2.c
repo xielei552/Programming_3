19456af9b6d253a4e8dff418c517719f3c9996f1
Lei Xie <xielei552@xielei.laptop>
2014-09-17 23:51:54 -0300

    fixed 1 more problem

--- a2p2.c
+++ a2p2.c
@@ -39,6 +39,7 @@ int main(int argc, char* argv[])
                 if (i == 1)
                 {
                     MIN = number;
+                    MAX = number;
                 }
                 /*get maximum number*/
                 MIN = ((MIN < number) ? MIN : number);
